5b9ba6bbbde5a2efa67d8990c02b390fb371849d
M-hafez98 <muhammedhafez98@gmail.com>
2021-05-31 13:48:13 +0200

    this is a custom library for standard types and bit math operation

---
+++ 01-LIB/01-STD_TYPES/STD_TYPES.h
@@ -0,0 +1,20 @@
+/***********************************************************************************************************/
+/* Auther		: Mohammed Hafez																		   */
+/* Date 		: 19 Sep 2020																			   */
+/* Version		: 02																					   */
+/***********************************************************************************************************/
+
+#ifndef STD_TYPES
+#define STD_TYPES
+
+typedef unsigned char		u8;
+typedef unsigned short int 	u16;
+typedef unsigned long int	u32;
+
+typedef signed char			s8;
+typedef signed short int 	s16;
+typedef signed long int		s32;
+
+#define NULL (void* 0)
+
+#endif
